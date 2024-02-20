fa47b50fec6e640a762c6614b0de9dbea1d5946f
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-02-20 00:27:01 -0300

    add handle errors async

--- src/http/controllers/create-user.ts
+++ src/http/controllers/create-user.ts
@@ -1,8 +1,15 @@
 import { makeCreateUserUseCase } from '@/services/factories/make-create-user-use-case'
 import type { Response, Request } from 'express'
+import z from 'zod'
+
+const createUserBodySchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  password: z.string().min(6),
+})
 
 export async function CreateUserController(req: Request, res: Response) {
-  const { email, name, password } = req.body
+  const { email, name, password } = createUserBodySchema.parse(req.body)
   const createUserUseCase = makeCreateUserUseCase()
   try {
     const { user } = await createUserUseCase.execute({ email, name, password })
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,44 @@
-import express from 'express'
+import 'express-async-errors'
+import express, { NextFunction, Request, Response } from 'express'
 import { env } from './env/index'
 import { router } from './http/routes'
 import { mongoClient } from './database/mongo'
+import { ZodError } from 'zod'
 
 const app = express()
+
+// Middleware para parsear o corpo das requisições como JSON
 app.use(express.json())
 
+// Rotas da aplicação
 app.use(router)
+
+// Conexão com o MongoDB
 ;(async () => {
   try {
     await mongoClient.connect()
+    console.log('Connected to MongoDB!')
   } catch (error) {
-    console.log(error)
+    console.error('Failed to connect to MongoDB:', error)
+    process.exit(1) // Encerra o processo se a conexão falhar
   }
-  const httpServer = app.listen(env.PORT, () => {
-    console.log('Running!')
-  })
-  httpServer.on('close', async () => {
-    await mongoClient.disconnect()
-  })
 })()
+
+// Middleware de tratamento de erros
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (error: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (error instanceof ZodError) {
+      return response
+        .status(400)
+        .json({ message: 'Validation error', issues: error.format() })
+    } else {
+      return response.status(500).json({ message: 'Internal Server Error' })
+    }
+  },
+)
+
+// Inicialização do servidor
+app.listen(env.PORT, () => {
+  console.log(`Server running on port ${env.PORT}!`)
+})
